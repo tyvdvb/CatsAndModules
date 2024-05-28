0a989097fb83fa9e5603779c0fece0d641b57359
tyvdvb <ira1980nazar@gmail.com>
2024-05-28 16:11:20 -0400

    Initial Commit

---
+++ CatsAndModules_IrynaNazar/CatsAndModules_IrynaNazarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatsAndModules_IrynaNazarApp.swift
+//  CatsAndModules_IrynaNazar
+//
+//  Created by Ira Nazar on 2024-05-28.
+//
+
+import SwiftUI
+
+@main
+struct CatsAndModules_IrynaNazarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CatsAndModules_IrynaNazar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CatsAndModules_IrynaNazar
+//
+//  Created by Ira Nazar on 2024-05-28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
